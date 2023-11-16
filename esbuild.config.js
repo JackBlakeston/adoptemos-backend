78b64753389763434644b529016600900575e776
Jack Blakeston <jackblakeston93@gmail.com>
2023-11-16 20:17:24 +0100

    Add esbuild bundler

---
+++ esbuild.config.js
@@ -0,0 +1,16 @@
+const esbuild = require('esbuild');
+const path = require('path');
+
+esbuild
+  .build({
+    entryPoints: ['./src/main.ts'],
+    bundle: true,
+    outfile: './lib/main.js',
+    platform: 'node',
+    format: 'cjs',
+    sourcemap: true,
+    target: 'node18',
+    tsconfig: './tsconfig.json',
+    external: ['express', 'mongoose'],
+  })
+  .catch(() => process.exit(1));
