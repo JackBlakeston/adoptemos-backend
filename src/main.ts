392ce788bfb234f93026dfe19f4d44ec1bcf2c84
Jack Blakeston <jackblakeston93@gmail.com>
2023-11-16 20:39:49 +0100

    Set up basic server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,38 @@
-console.log('hey');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-const test: number = 0;
+// Import your API routes and controllers here
+// Example:
+// import { dogsRoutes } from './routes/dogs';
+
+// Initialize Express
+const app = express();
+
+// Middleware
+app.use(express.json()); // Parse JSON request bodies
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+
+// Connect to MongoDB using Mongoose
+mongoose.connect('mongodb://localhost/adoptemos');
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define your API routes and attach them to the Express app
+// Example:
+// app.use('/dogs', dogsRoutes);
+
+// Define a default route
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Dog Adoption API!' });
+});
+
+// Start the Express server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
