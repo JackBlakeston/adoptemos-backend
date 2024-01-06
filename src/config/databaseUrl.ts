c89e076a282950060ebf153108c191a39288e0be
Jack Blakeston <jackblakeston93@gmail.com>
2024-01-06 21:42:47 +0100

    AS-029: Modify IS_PROD env variable
    Rename IS_PROD to ENVIRONMENT and make string instead of boolean

--- src/config/databaseUrl.ts
+++ src/config/databaseUrl.ts
@@ -1,6 +1,8 @@
-const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, IS_PROD, MONGODB_PORT, MONGODB_NAME } = process.env;
+const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, ENVIRONMENT, MONGODB_PORT, MONGODB_NAME } = process.env;
 
 const DB_URL_PROD = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}?retryWrites=true&w=majority`;
 const DB_URL_DEV = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}?authSource=admin`;
 
-export const DB_URL = IS_PROD ? DB_URL_PROD : DB_URL_DEV;
+const isProd = ENVIRONMENT === 'prod';
+
+export const DB_URL = isProd ? DB_URL_PROD : DB_URL_DEV;
