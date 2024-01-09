bc7244e8b41618191e3ec72b6402e1fa5fe22136
Jack Blakeston <jackblakeston93@gmail.com>
2024-01-09 22:23:28 +0100

    AS-007: Add OpenApiService
    Add services to Server

--- src/infrastructure/server/Server.ts
+++ src/infrastructure/server/Server.ts
@@ -2,6 +2,8 @@ import express, { Express } from 'express';
 import swaggerUi from 'swagger-ui-express';
 
 import { MainRouter } from '@src/infrastructure/routes/MainRouter/MainRouter';
+import { OpenApiService } from '@src/infrastructure/services/OpenApiService/OpenApiService';
+import { StorageProviderService } from '@src/infrastructure/services/StorageProviderService/StorageProviderService';
 
 import { getSwaggerDocument } from '@src/docs/getSwaggerDocument';
 import swaggerUiOptions from '@src/docs/swaggerUiConfig.json';
@@ -14,10 +16,20 @@ export class Server {
     this.app = express();
     this.router = new MainRouter();
 
-    this.app.use(express.json());
+    this.configureMiddleware();
+    this.initializeServices();
     this.configureSwagger();
     this.configureRoutes();
   }
+
+  private initializeServices = () => {
+    StorageProviderService.initialize();
+    OpenApiService.initialize(this.app);
+  };
+
+  private configureMiddleware = () => {
+    this.app.use(express.json({ limit: '5mb' }));
+  };
 
   private configureSwagger = () => {
     const swaggerDocument = getSwaggerDocument();
