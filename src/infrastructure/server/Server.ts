3a3f16ac450bb1e103ed24d63cb0f859b75febec
Jack Blakeston <jackblakeston93@gmail.com>
2024-01-11 20:39:21 +0100

    AS-039: Add jest coverage

--- src/infrastructure/server/Server.ts
+++ src/infrastructure/server/Server.ts
@@ -1,12 +1,8 @@
 import express, { Express } from 'express';
-import swaggerUi from 'swagger-ui-express';
 
 import { MainRouter } from '@src/infrastructure/routes/MainRouter/MainRouter';
 import { OpenApiService } from '@src/infrastructure/services/OpenApiService/OpenApiService';
 import { StorageProviderService } from '@src/infrastructure/services/StorageProviderService/StorageProviderService';
-
-import { getSwaggerDocument } from '@src/docs/getSwaggerDocument';
-import swaggerUiOptions from '@src/docs/swaggerUiConfig.json';
 
 export class Server {
   private app: Express;
@@ -18,7 +14,6 @@ export class Server {
 
     this.configureMiddleware();
     this.initializeServices();
-    this.configureSwagger();
     this.configureRoutes();
   }
 
@@ -29,11 +24,6 @@ export class Server {
 
   private configureMiddleware = () => {
     this.app.use(express.json({ limit: '5mb' }));
-  };
-
-  private configureSwagger = () => {
-    const swaggerDocument = getSwaggerDocument();
-    this.app.use('/openapi', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerUiOptions));
   };
 
   private configureRoutes = (): void => {
