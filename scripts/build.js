9b254ac1beedf4f1de4644e29a958701a2750689
Jack Blakeston <jackblakeston93@gmail.com>
2024-01-14 02:22:42 +0100

    AS-045: Fix firebase esbuild error workaround

--- scripts/build.js
+++ scripts/build.js
@@ -1,12 +1,6 @@
 import esbuild from 'esbuild';
 import { runStyledScript } from './scripUtils/styledScript.js';
 
-export const OPTIONS_TO_FIX_ESBUILD_DYNAMIC_REQUIRE_BUG = {
-  banner: {
-    js: "const require = (await import('node:module')).createRequire(import.meta.url);const __filename = (await import('node:url')).fileURLToPath(import.meta.url);const __dirname = (await import('node:path')).dirname(__filename);",
-  },
-};
-
 const buildFiles = async () => {
   await esbuild.build({
     entryPoints: ['./src/main.ts'],
@@ -17,8 +11,7 @@ const buildFiles = async () => {
     sourcemap: true,
     target: 'node18',
     tsconfig: './tsconfig.json',
-    external: ['express', 'mongoose', 'swagger-ui-express'],
-    ...OPTIONS_TO_FIX_ESBUILD_DYNAMIC_REQUIRE_BUG,
+    external: ['express', 'mongoose', 'swagger-ui-express', 'firebase-admin'],
   });
 };
 
