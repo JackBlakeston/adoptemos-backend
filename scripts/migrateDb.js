1a3a64253f23f107d9bd63434e07fddf5774218e
Jack Blakeston <jackblakeston93@gmail.com>
2024-01-17 01:03:01 +0100

    AS-051: Modify migrations to be able to run from container

--- scripts/migrateDb.js
+++ scripts/migrateDb.js
@@ -2,11 +2,13 @@ import { runStyledScript } from './scripUtils/styledScript.js';
 import { runShellCommand } from './scripUtils/runShellCommand.js';
 
 const command = process.argv[2];
+const isFromContainer = process.argv[3] === '--container';
 
 const runMigrations = async () => {
-  await runShellCommand(
-    `docker exec adoptemos-server node scripts/buildMigrations.js && docker exec adoptemos-server npx migrate-mongo ${command} -f lib/src/config/migrations.cjs`,
-  );
+  const shellCommand = isFromContainer
+    ? `node scripts/buildMigrations.js && npx migrate-mongo ${command} -f lib/src/config/migrations.cjs`
+    : `docker exec adoptemos-server node scripts/buildMigrations.js && docker exec adoptemos-server npx migrate-mongo ${command} -f lib/src/config/migrations.cjs`;
+  await runShellCommand(shellCommand);
 };
 
 runStyledScript({
