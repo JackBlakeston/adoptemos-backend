834cc5b6647605e4048289bc3fc361443aa69167
Jack Blakeston <jackblakeston93@gmail.com>
2024-01-22 18:21:02 +0100

    AS-058: Add test database url
    Modify docker-compose to run mongodb in the actual specified port
    
    Modify MONGODB_HOST env variable to correct default one

--- src/config/databaseUrl.ts
+++ src/config/databaseUrl.ts
@@ -3,6 +3,4 @@ const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, ENVIRONMENT, MONGODB_PORT,
 const DB_URL_PROD = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}?retryWrites=true&w=majority`;
 const DB_URL_DEV = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}?authSource=admin`;
 
-const isProd = ENVIRONMENT === 'prod';
-
-export const DB_URL = isProd ? DB_URL_PROD : DB_URL_DEV;
+export const DB_URL = ENVIRONMENT === 'prod' ? DB_URL_PROD : DB_URL_DEV;
